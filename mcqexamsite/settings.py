1fdb30d90201dccaf0aea8bea9a56246d438112d
Amit Kharel <ajeetkharell@gmail.com>
2020-10-10 10:14:15 +0545

    Minor changes

--- mcqexamsite/settings.py
+++ mcqexamsite/settings.py
@@ -123,7 +123,7 @@ LOGOUT_REDIRECT_URL = 'user-login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
